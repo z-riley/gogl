5b4619385e52cd37160f9ecbcd5daf8dcafec745
zac460 <zac460@hotmail.co.uk>
2024-08-08 17:17:54 +0100

    Neaten shapes example

--- examples/shapes/main.go
+++ examples/shapes/main.go
@@ -24,42 +24,42 @@ func main() {
 	second := time.Tick(time.Second)
 
 	// Shapes
-	rect := tgl.NewRect(
+	rectSolid := tgl.NewRect(
 		120, 90,
-		tgl.Vec{X: 200, Y: 200},
+		tgl.Vec{X: 50, Y: 50},
 		tgl.WithStyle(tgl.Style{Colour: color.RGBA{0, 0, 255, 1}, Thickness: 0}),
 	)
-	rect2 := tgl.NewRect(
+	rectOutline := tgl.NewRect(
 		120, 90,
-		tgl.Vec{X: 200, Y: 200},
-		tgl.WithStyle(tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 4}),
+		tgl.Vec{X: 50, Y: 50},
+		tgl.WithStyle(tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 2}),
 	)
 	circle := tgl.NewCircle(
 		100,
-		tgl.Vec{X: 500, Y: 200},
-		tgl.WithStyle(tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 10}),
+		tgl.Vec{X: 300, Y: 100},
+		tgl.WithStyle(tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 30}),
 	)
-	txt := tgl.NewText("Hello there", tgl.Vec{X: 100, Y: 600}).
-		SetColour(color.RGBA{255, 255, 255, 255})
 	triangle := tgl.NewTriangle(
-		tgl.Vec{X: 100, Y: 400},
-		tgl.Vec{X: 800, Y: 600},
-		tgl.Vec{X: 400, Y: 80},
+		tgl.Vec{X: 400, Y: 50},
+		tgl.Vec{X: 580, Y: 120},
+		tgl.Vec{X: 450, Y: 130},
 	).SetStyle(tgl.Style{Colour: color.RGBA{100, 10, 100, 255}})
+	txt := tgl.NewText("Hello there", tgl.Vec{X: 800, Y: 80}).
+		SetColour(color.RGBA{255, 255, 255, 255})
 
 	// Keybinds
 	win.RegisterKeybind(tgl.KeyEscape, func() { win.Quit() })
 	win.RegisterKeybind(tgl.KeyLCtrl, func() { win.Quit() })
-	win.RegisterKeybind(tgl.KeyE, func() { rect.Move(tgl.Vec{X: 2, Y: 2}) })
+	win.RegisterKeybind(tgl.KeyE, func() { rectSolid.Move(tgl.Vec{X: 2, Y: 2}) })
 
 	for win.IsRunning() {
 		win.Framebuffer.SetBackground(color.RGBA{39, 45, 53, 255})
 
 		// Draw shapes
-		win.Draw(triangle)
-		win.Draw(rect)
-		win.Draw(rect2)
+		win.Draw(rectSolid)
+		win.Draw(rectOutline)
 		win.Draw(circle)
+		win.Draw(triangle)
 		win.Draw(txt)
 
 		win.SetTitle(fmt.Sprint(win.MouseLocation()))
