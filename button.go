f98b70d2c171c2d387c5878676df7c7aaf1ccea9
zac460 <zac460@hotmail.co.uk>
2024-08-09 05:25:30 +0100

    Fill out button functionality

--- button.go
+++ button.go
@@ -1,38 +1,71 @@
 package turdgl
 
-type hoverable interface {
+type button interface {
+	Draw(*FrameBuffer)
 	IsHovering(Vec) bool
 }
 
+// Button can be build on top of shapes to create pressable buttons.
 type Button struct {
-	shape          hoverable
-	cb             func()     // the callback function to execute on press
-	prevMouseState MouseState // the previous mouse button state
+	Shape          button           // the base shape the button is built on
+	CB             func(MouseState) // the callback function to execute on press
+	Trigger        MouseState       // which mouse button must be used to press the button
+	Behaviour      ButtonBehaviour  // how the button responds to being pressed
+	prevMouseState MouseState       // the previous mouse button state
 }
 
 // NewButton constructs a new button from any shape that satisfies
 // the hoverable interface.
-func NewButton(shape hoverable, callback func()) *Button {
+func NewButton(shape button, callback func(MouseState)) *Button {
 	return &Button{
-		shape:          shape,
-		cb:             callback,
-		prevMouseState: NoClick,
+		Shape:     shape,
+		CB:        callback,
+		Trigger:   LeftClick,
+		Behaviour: OnPressAndRelease,
 	}
 }
+
+// Draw draws the button's base shape to the provided frame buffer.
+func (b *Button) Draw(buf *FrameBuffer) {
+	b.Shape.Draw(buf)
+}
+
+// ButtonBehaviour represents how a button responds to being pressed.
+type ButtonBehaviour int
+
+const (
+	OnPress           ButtonBehaviour = iota // execute behaviour on press
+	OnRelease                                // execute behaviour on release
+	OnPressAndRelease                        // execute behaviour on press and release
+	OnHold                                   // execute behaviour as long as button is held down
+)
 
 // Update examines button state and executes behaviour accordingly.
 func (b *Button) Update(win *Window) {
 
-	// TODO: implement different behaviour models
-	// - Execute once on left/right click
-	// - Execute continously on left/right click
+	currentMouseState := win.MouseButtonState()
+	hovering := b.Shape.IsHovering(win.MouseLocation())
 
-	// THIS LOGIC WORKS FOR SINGLE CLICK BEHAVIOUR
-	leftClicked := b.prevMouseState == NoClick && win.MouseButtonState() == LeftClick
-	if leftClicked {
-		b.prevMouseState = LeftClick
-		b.cb()
-	} else if win.MouseButtonState() == NoClick {
-		b.prevMouseState = NoClick
+	switch b.Behaviour {
+	case OnPress:
+		if hovering && (b.prevMouseState == NoClick && currentMouseState == b.Trigger) {
+			b.CB(currentMouseState)
+		}
+	case OnRelease:
+		if hovering && (b.prevMouseState == b.Trigger && currentMouseState == NoClick) {
+			b.CB(currentMouseState)
+		}
+	case OnPressAndRelease:
+		if hovering && (b.prevMouseState != currentMouseState) {
+			b.CB(currentMouseState)
+		}
+	case OnHold:
+		if hovering && (currentMouseState == b.Trigger) {
+			b.CB(currentMouseState)
+		}
+	default:
+		panic("unsupported button behaviour")
 	}
+
+	b.prevMouseState = win.MouseButtonState()
 }
--- examples/shapes/main.go
+++ examples/shapes/main.go
@@ -23,7 +23,7 @@ func main() {
 	frames := 0
 	second := time.Tick(time.Second)
 
-	// Shapes
+	// Shapes can be filled or have an outline of specified thickness
 	rectSolid := tgl.NewRect(
 		120, 90,
 		tgl.Vec{X: 50, Y: 50},
@@ -34,21 +34,31 @@ func main() {
 		tgl.Vec{X: 50, Y: 50},
 		tgl.WithStyle(tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 2}),
 	)
-	circle := tgl.NewCircle(
-		100,
-		tgl.Vec{X: 300, Y: 100},
-		tgl.WithStyle(tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 30}),
-	)
-	circleButton := tgl.NewButton(circle, func() { fmt.Println("Circle pressed") })
+
+	// Buttons can be constructed from shapes
+	styleUnpressed := tgl.Style{Colour: color.RGBA{80, 0, 0, 1}, Thickness: 30}
+	stylePressed := tgl.Style{Colour: color.RGBA{255, 0, 0, 1}, Thickness: 0}
+	c := tgl.NewCircle(100, tgl.Vec{X: 300, Y: 100}, tgl.WithStyle(styleUnpressed))
+	circleButton := tgl.NewButton(c, func(m tgl.MouseState) {
+		fmt.Println(time.Now())
+		if m == tgl.LeftClick {
+			c.SetStyle(stylePressed)
+		} else {
+			c.SetStyle(styleUnpressed)
+		}
+	})
+
+	// More complex shapes with limited feature sets can also be created
 	triangle := tgl.NewTriangle(
 		tgl.Vec{X: 400, Y: 50},
 		tgl.Vec{X: 580, Y: 120},
 		tgl.Vec{X: 450, Y: 130},
 	).SetStyle(tgl.Style{Colour: color.RGBA{100, 10, 100, 255}})
+
 	txt := tgl.NewText("Hello there", tgl.Vec{X: 800, Y: 80}).
 		SetColour(color.RGBA{255, 255, 255, 255})
 
-	// Keybinds
+	// Register window-level keybinds
 	win.RegisterKeybind(tgl.KeyEscape, func() { win.Quit() })
 	win.RegisterKeybind(tgl.KeyLCtrl, func() { win.Quit() })
 	win.RegisterKeybind(tgl.KeyE, func() { rectSolid.Move(tgl.Vec{X: 2, Y: 2}) })
@@ -59,14 +69,13 @@ func main() {
 		// Draw shapes
 		win.Draw(rectSolid)
 		win.Draw(rectOutline)
-		win.Draw(circle)
 		win.Draw(triangle)
 		win.Draw(txt)
-
-		circleButton.Update(win)
+		win.Draw(circleButton)
 
 		win.SetTitle(fmt.Sprint(win.MouseLocation()))
 
+		circleButton.Update(win)
 		win.Update()
 
 		// Count FPS
