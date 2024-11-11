493f8ad4a7dd82c00914602b922244a6b9cdfa77
Zac <zac460@hotmail.co.uk>
2024-11-11 21:00:07 +0000

    Set default text box text alignment to centre

--- textbox.go
+++ textbox.go
@@ -20,7 +20,7 @@ type TextBox struct {
 func NewTextBox(shape hoverable, text, fontPath string) *TextBox {
 	return &TextBox{
 		Shape:        shape,
-		Text:         NewText(text, shape.GetPos(), fontPath),
+		Text:         NewText(text, shape.GetPos(), fontPath).SetAlignment(AlignCentre),
 		selectedCB:   func() {},
 		deselectedCB: func() {},
 		modifiedCB:   func() {},
